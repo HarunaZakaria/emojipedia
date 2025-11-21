715a57a5ec4ec915cd086335590f6e5cbfc5bfaf
Harunzy <Harunzy55@gmail.com>
2025-11-21 01:07:27 +0000

    add header.jsx

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>
+        Emojipedia <span>📖</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
