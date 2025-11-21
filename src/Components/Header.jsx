f55d75abc4f75354afff362e0c9a56899b3cab04
Harunzy <Harunzy55@gmail.com>
2025-11-21 01:58:19 +0000

    add search component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './NavBar';
+import Search from './Search';
 
 const Header = () => {
   return (
     <div>
       <NavBar />
+      <Search />
     </div>
   );
 };
