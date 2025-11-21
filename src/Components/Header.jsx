cea72a83921bae711d63700020226bda501d8e79
Harunzy <Harunzy55@gmail.com>
2025-11-21 01:48:26 +0000

    add navbar component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import NavBar from './NavBar';
 
 const Header = () => {
   return (
     <div>
-      <h1>
-        Emojipedia <span>📖</span>
-      </h1>
+      <NavBar />
     </div>
   );
 };
