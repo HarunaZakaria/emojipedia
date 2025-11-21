5fa9ec9600a207666c2a3c8cd84459ff40fa528b
Harunzy <Harunzy55@gmail.com>
2025-11-21 01:10:17 +0000

    add emojipedia.jsx

---
+++ src/Components/Emojipedia.jsx
@@ -0,0 +1,108 @@
+const emojipedia = [
+  {
+    id: 1,
+    name: 'Red Heart',
+    emoji: '❤️',
+    meaning:
+      'The red heart ideograph is traditionally used for expressions of love and romance across many cultures, with this being amongst the most frequence use cases for this emoji.',
+  },
+  {
+    id: 2,
+    name: 'Person Standing',
+    emoji: '🧍',
+    meaning:
+      'A person standing upright. Commonly used to convey awkwardness, particularly on TikTok.Similar in appearance to 🚶 Person Walking, but shown facing forward and without any motion implied.',
+  },
+  {
+    id: 3,
+    name: 'Woman and Man Holding Hands',
+    emoji: '👫',
+    meaning:
+      'A woman and man holding hands, most likely due to being in a relationship.',
+  },
+  {
+    id: 4,
+    name: 'Couple with Heart',
+    emoji: '💑',
+    meaning:
+      'A couple standing next to each other, smiling, with a love heart floating in the air between them. No gender is specified for the two people, and as such this may be used to represent the concept of a couple broadly, and/or as a means of showing two non-binary people.',
+  },
+  {
+    id: 5,
+    name: 'Kiss Mark',
+    emoji: '💋',
+    meaning:
+      'The mark left after a firm kiss is placed with bright lipstick. Used in place of ‘xxx’ (kisses), or to send a kiss to someone. More seductive than the kiss emoji.',
+  },
+  {
+    id: 6,
+    name: 'Smiling Face with Hearts',
+    emoji: '🥰',
+    meaning:
+      'A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love. The placement of the hearts varies slightly by platform',
+  },
+  {
+    id: 7,
+    name: 'Revolving Hearts',
+    emoji: '💞',
+    meaning:
+      'Hearts revolving around one or more other hearts. This emoji was originally animated on a number of Japanese devices.',
+  },
+  {
+    id: 8,
+    name: 'Heart Hands',
+    emoji: '🫶',
+    meaning:
+      'Two hands forming a heart shape. Used to express love and support.This emoji currently showns the heart being made through two hands being curved into a C-shape with their thumbs touching. This is stereotypically referred to as the "Millennial Hand Heart".',
+  },
+  {
+    id: 9,
+    name: 'Backhand Index Pointing Right',
+    emoji: '👉',
+    meaning: 'An index finger pointing to the right.',
+  },
+  {
+    id: 10,
+    name: 'Party Popper',
+    emoji: '🎉',
+    meaning:
+      'A party popper, as explodes in a shower of confetti and streamers at a celebration. Generally depicted as a gold party hat, striped on some platforms, at a 45° angle, with multicolored confetti and streamers coming from its opening at the upper right. Twitter’s cone is red.',
+  },
+  {
+    id: 11,
+    name: 'Smiling Face with Smiling Eyes',
+    emoji: '😊',
+    meaning:
+      'A yellow face with smiling eyes and a broad, closed smile turning up to rosy cheeks. Often expresses genuine happiness and warm, positive feelings.',
+  },
+  {
+    id: 12,
+    name: 'Wrapped Gift',
+    emoji: '🎁',
+    meaning:
+      'A present or gift in a box tied with a bow, as given on a birthday or Christmas. Box and ribbon color vary across platforms, but commonly depicted as a gold box with a lid wrapped with a red ribbon.',
+  },
+  {
+    id: 13,
+    name: 'Fire ',
+    emoji: '🔥',
+    meaning:
+      'A flame, as produced when something is on fire. Depicted as a red, orange, and yellow flickering flame.',
+  },
+  {
+    id: 14,
+    name: 'Face with Tears of Joy',
+    emoji: '😂',
+    meaning:
+      'A yellow face with a big grin, uplifted eyebrows, and smiling eyes, each shedding a tear from laughing so hard. Widely used to show something is funny or pleasing. ',
+  },
+  {
+    id: 15,
+    name: 'Loudly Crying Face',
+    emoji: '😭',
+    meaning:
+      'A yellow face with an open mouth wailing and streams of heavy tears flowing from closed eyes. May convey inconsolable grief but also other intense feelings, such as uncontrollable laughter, pride or overwhelming joy.',
+  },
+];
+
+export default emojipedia;
