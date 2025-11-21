c51b9a78a502acb84036546be983b05b4e0d8a2f
Harunzy <Harunzy55@gmail.com>
2025-11-21 03:30:47 +0000

    add emocard comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-
-import './App.css'
-import Header from './Components/Header'
+import './App.css';
+import EmoCard from './Components/EmoCard';
+import Header from './Components/Header';
 
 function App() {
-
   return (
     <>
       <header>
         <Header />
       </header>
+      <main>
+        <EmoCard
+          emoji="❤️"
+          name="Red Heart"
+          meaning="The red heart ideograph is traditionally used for expressions of love and romance across many cultures"
+        />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
